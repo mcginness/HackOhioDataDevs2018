8ee30bc89c6ba6f0b626b2cd1c82ae513956a54d
Hrishi <saraiya.7@osu.edu>
2018-10-27 17:10:51 -0400

    stuff to check

---
+++ finalTest.py
@@ -0,0 +1,14 @@
+from pandas import DataFrame, read_csv
+from bs4 import BeautifulSoup
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import matplotlib
+import requests
+import re
+from seperator import *
+from SongDataScrape import*
+
+getSongLyricsToTXT("Rap God", "Eminem")
+printPairs(pullWords("Rap-God-lyrics"), "Rap God")
